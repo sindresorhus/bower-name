be76f0802403abf7719318108754e8509e4323c6
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-18 15:23:23 +0200

    lowercase the name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var got = require('got');
 var endpoint = 'https://bower.herokuapp.com/packages/';
 
 module.exports = function (name, cb) {
-	got.head(endpoint + encodeURIComponent(name), function (err, data) {
+	got.head(endpoint + encodeURIComponent(name.toLowerCase()), function (err, data) {
 		if (err && err.code === 404) {
 			cb(null, true);
 			return;
