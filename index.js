80c415a5d53d427242b7822b8e03f6fa005ab93a
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-19 18:37:23 +0700

    Close #1 PR: Promisify.

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 'use strict';
 var got = require('got');
+var Promise = require('pinkie-promise');
 var endpoint = 'https://bower.herokuapp.com/packages/';
 
-module.exports = function (name, cb) {
+module.exports = function (name) {
 	if (!(typeof name === 'string' && name.length !== 0)) {
-		throw new Error('Package name required');
+		return Promise.reject(new Error('Package name required'));
 	}
 
-	got.head(endpoint + encodeURIComponent(name.toLowerCase()), function (err) {
+	return got.head(endpoint + encodeURIComponent(name.toLowerCase())).then(function () {
+		return false;
+	}).catch(function (err) {
 		if (err && err.statusCode === 404) {
-			cb(null, true);
-			return;
+			return true;
 		}
 
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb(null, false);
+		throw err;
 	});
 };
